7750c43ea08d5815b97c3462f18565db1d8a1ca3
mariovzc <mario.vizcainos@gmail.com>
2020-10-01 18:10:27 -0500

    refactor view

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,25 +1,3 @@
 import React from 'react';
 
-const Home = () => {
-    return(
-        <h1>Accounts</h1>
-    )
-}
-export default Home;
-
-
-
-/* export default class Home extends BaseView {
-    constructor() {
-        super();
-        this.state = {
-            value: null,
-        };
-    }
-
-    static_content() {
-        return ( 
-            <h1>Hola soy la pagina de home</h1>
-        );
-    }
-} */
+export default () => <h1>Accounts</h1>
--- src/views/Teams.js
+++ src/views/Teams.js
@@ -1,28 +1,3 @@
 import React from 'react';
-const Teams = () => {
-    return(
-        <h1>Teams</h1>
-    )
-}
+export default () => <h1>Teams</h1>
 
-export default Teams;
-
-/* export default class Teams extends BaseView {
-    constructor() {
-        super();
-        this.state = {
-            value: null,
-        };
-        this.can_create = false;
-        this.can_edit = false;
-        this.can_delete = false;
-    }
-
-    get_columns() {
-        return [
-            { "label": "Username", "key": "username", "sortable": true, "filters": ["like", "equals", "not equals"] },
-            { "label": "Name", "key": "name" },
-            { "label": "Country", "key": "country_id" },
-        ];
-    }
-} */
